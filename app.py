f2759a3df51f27cd09297900e397a5d057d37157
Krishnendu Mukherjee <kmukherjeejr@gmail.com>
2020-12-09 15:01:50 +0530

    First upload of the project

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about-us')
+def aboutus():
+    return render_template('about.html')
+
+@app.route('/contact-us')
+def contactus():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
