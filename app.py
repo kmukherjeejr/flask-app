c5cad267d6f68bb7be85c167525eeb087837d4f1
Krishnendu Mukherjee <kmukherjeejr@gmail.com>
2020-12-11 12:40:08 +0530

    Removed unnecessary files

--- app.py
+++ app.py
@@ -2,25 +2,32 @@ from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 @app.route('/about-us')
-def aboutus():
+def about_us():
     return render_template('about.html')
+
 
 @app.route('/contact-us')
 def contactus():
     return render_template('contact.html')
 
-@app.route('/contact-details', methods=['POST','GET'])
-def contactdetails():
+
+@app.route('/contact-details', methods=['POST', 'GET'])
+def contact_details():
     data = request.form
-    return render_template("contactdetails.html", data = data)
+    return render_template("contactdetails.html", data=data)
+
+
 @app.route('/going-back', methods=['POST'])
-def goingback():
+def going_back():
     return redirect(url_for('contactus'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
